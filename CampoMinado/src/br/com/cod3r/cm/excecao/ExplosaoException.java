648014121554d4ac5512524e12efd473832cb202
HenriqueDiulio <henriquediulio3@gmail@gmail.com>
2024-08-20 22:35:58 -0300

    Primeira Versão

---
+++ CampoMinado/src/br/com/cod3r/cm/excecao/ExplosaoException.java
@@ -0,0 +1,10 @@
+package br.com.cod3r.cm.excecao;
+
+public class ExplosaoException extends RuntimeException {
+
+	/**
+	 * Variavel long declarada para tirar o alerta em ExplosaoException
+	 */
+	private static final long serialVersionUID = -4546262480933252614L;
+
+}
